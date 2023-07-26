ec1fa4338d83aa9a09f67509a02b795626aac02c
monal12 <monalkapoor13113@gmail.com>
2023-07-27 02:48:07 +0530

    adding more features

--- src/app/MyComponents/add-todo/add-todo.component.ts
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -9,12 +9,16 @@ import { dailytask } from 'src/app/dailytask';
 export class AddTodoComponent {
   title:string;
   desc:string;
+  duedate:Date;
+  priority:string;
   @Output() todoAdd: EventEmitter<dailytask>= new EventEmitter();
   onSubmit(){
     const todo={
       sno: 8,
       title: this.title,
       desc: this.desc,
+      duedate:this.duedate,
+      priority: this.priority,
       active: true
     }
     this.todoAdd.emit(todo)
--- src/app/dailytask.ts
+++ src/app/dailytask.ts
@@ -1,6 +1,8 @@
 export class dailytask{
     sno: number
     title: string
-    desc: string
+    desc: string 
+    duedate: Date
+    priority: string
     active: boolean
 }
